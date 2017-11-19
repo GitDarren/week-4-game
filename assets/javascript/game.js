9c7a88693fd1a4d68225347e60fa89601ded67df
GitDarren <darren321@gmail.com>
2017-11-19 09:02:18 -0600

    changed the theme and started creating the javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,59 @@
+//Crystal Game - turned into a Peanuts Game
+
+//Vars to generated random Numbers
+var daysLeft = Math.floor(Math.random() * (12-1+1)) + 1;
+var charliebrown = Math.floor(Math.random() * 11) +1;
+var lucy = Math.floor(Math.random() * 11) +1;
+var snoopuy= Math.floor(Math.random() * 11) +1;
+var linus = Math.floor(Math.random() * 11) +1;
+
+//Variables for keeping track of the game
+var wins;
+var loses;
+var daysGuessed;
+
+
+
+
+
+//Create the game Setup here
+//Populate the DOM with a randomly generated daysleft
+//Populate the DOM 
+
+//So using document.ready to load the game!//
+$(document).ready(function() {
+
+
+
+function loadGame()
+$('#daysLeft').text(daysLeft);
+
+
+
+function playGame() 
+
+
+
+
+
+
+
+
+
+function checkWin()
+
+
+
+
+
+
+function checkLoss()
+
+
+
+
+
+
+
+
+function reset()
