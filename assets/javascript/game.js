cff86e29b7b52294dc6576388c76adda845d7f7d
GitDarren <darren321@gmail.com>
2017-11-21 10:08:08 -0600

    trying to fix scope issues, not having much success

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,156 +1,113 @@
 //Crystal Game - turned into a Peanuts Game
 
 
+//This game is a variation on the Crystals Game. I changed the game so that users' goal is to reduce the randomly generated number
+//by clicking one of the Peanuts characters to get the total to Zero "Days Left".  
+//I've set up all the variables needed for the game 
 
 
-
-
-//To generate the random number (daysleft)//
-
-
-
-
-//Vars to generated
-// daysLeft(19, 120);
-// console.log(daysLeft(19, 120));
-// var charliebrown = Math.floor(Math.random() * 11) + 1;
-// var lucy = Math.floor(Math.random() * 11) + 1;
-// var snoopuy = Math.floor(Math.random() * 11) + 1;
-// var linus = Math.floor(Math.random() * 11) + 1;
 
 //Variables for keeping track of the game
 var wins = 0;
 var loses = 0;
-var daysGuessed = 0;
+var total; 
 
-
-
-
-
-//Create the game Setup here
-//Populate the DOM with a randomly generated daysleft
-//Populate the DOM 
 
 //Using document.ready to load the game!//
 $(document).ready(function () {
     console.log("ready!");
 
-    //Need to set daysLeft as a global variable so that both loadGame and playGame can use it//
-    function daysLeft(min, max) {
+    //Need to set total as a global variable so that both loadGame and playGame can use it//
+    function total(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    // var total = daysLeft(19, 120); 
+    total = total(19, 120);
+    console.log(total);
 
-    //Setting up the game with days left total, wins totals, loses totals, 
-    // function loadGame() {
-        
-    //     console.log(total);
-        // $("#daysLeft").html(total);
-        // console.log("how many" + '#daysleft');
-        // $("#wins").html('<h1>' + "Wins: " + wins + '</h1>');
-        // console.log("here are my " + wins + " wins");
-        // $("#loses").html('<h1>' + "Loses: " + loses + '</h1>');
-    // }
 
-    // loadGame();
-
-    function playGame() {
-
-            var total = daysLeft(19, 120); 
+    //loadGame sets up the game with days left total, wins totals, loses totals, 
+    function loadGame() {
 
         $("#daysLeft").html(total);
-        console.log("how many" + '#daysleft');
         $("#wins").html('<h1>' + "Wins: " + wins + '</h1>');
         console.log("here are my " + wins + " wins");
         $("#loses").html('<h1>' + "Loses: " + loses + '</h1>');
-        
+        console.log("here are my " + loses + " loses");
+
         //Setting up the variables for the the four Peanuts Characters!//
         var charliebrownVal = Math.floor(Math.random() * 11) + 1;
         var lucyVal = Math.floor(Math.random() * 11) + 1;
         var snoopyVal = Math.floor(Math.random() * 11) + 1;
         var linusVal = Math.floor(Math.random() * 11) + 1;
-        
+
         //Just checking to make sure the characters have values//
         console.log("Charlie Brown's Value " + charliebrownVal);
         console.log("Lucys's Value " + lucyVal);
         console.log("Snoopy's Value " + snoopyVal);
         console.log("Linus's Value " + linusVal);
+    }
 
-        //
+    //Starts the game on click of any of the peanuts characters//
+    function playGame() {
+
+        //Uses "this" to assign the click on each character.  
+        //However due to some scope issue, the console is showing the total number dropping after every click...
+        //But the total on the DOM is not reflecting the total change 
         $('.characters').on("click", function () {
             var thisId = $(this).attr('id');
-        if(thisId=== "charliebrown") {
-            total = total-charliebrownVal;
-            console.log(total);
-            console.log(charliebrownVal);
-            checkWin();
-            $('#daysLeft').html(total);
-        } else if(thisId=== "lucy") {
-            total = total-lucyVal;
-            console.log(total);
-            console.log(lucyVal);
-            checkWin();
-            $('#daysLeft').html(total);
-        } else if(thisId=== "snoopy") {
-            total = total-snoopyVal;
-            console.log(total);
-            console.log(snoopyVal);
-            checkWin();
-            $('#daysLeft').html(total);
-        } else if(thisId=== "linus") {
-            total = total-linusVal;
-            console.log(total);
-            console.log(linusVal);
-            checkWin();
-            $('#daysLeft').html(total);
-        } else  {
-            alert("Something Went Way Wrong");
-        }    
-        })
+            if (thisId === "charliebrown") {
+                total = total - charliebrownVal;
+                console.log(total);
+                console.log(charliebrownVal);
+                checkWin();
+                $('#daysLeft').html(total);
+            } else if (thisId === "lucy") {
+                total = total - lucyVal;
+                console.log(total);
+                console.log(lucyVal);
+                checkWin();
+                $('#daysLeft').html(total);
+            } else if (thisId === "snoopy") {
+                total = total - snoopyVal;
+                console.log(total);
+                console.log(snoopyVal);
+                checkWin();
+                $('#daysLeft').html(total);
+            } else if (thisId === "linus") {
+                total = total - linusVal;
+                console.log(total);
+                console.log(linusVal);
+                checkWin();
+                $('#daysLeft').html(total);
+            } else {
+                alert("Something Went Way Wrong");
+            }
+        });
+
+        loadGame();
+        playGame();
         
-    }
-    playGame();
+        //Checks the game to see if the user wins if the total equals Zero and tells the user they lose if they below Zero.  
+        //Still not working correct because the user can keep clicking characters and game keeps running. Not updating the Wins or Loses count on the DOM. 
+        function checkWin() {
+            if (total > 0) {
+                return;
+            } else {
+                if (total === 0) {
+                    wins++;
+                    $('#wins').html("Wins: " + wins);
+                    console.log("You Win!");
+                    alert("You Win!");
+                    loadGame();
 
+                } else {
+                    loses++;
+                    console.log("You Lose!");
+                    $('#loses').html("Loses: " + loses);
+                    loadGame();
 
-    function checkWin() {
-    if(total > 0)   {
-        return;
-    } else  {
-        if(total === 0) {
-            wins ++;
-            $('#wins').html("Wins: " + wins);
-            console.log("You Win!");
-            alert("You Win!");
-            playGame();
-
-            
-        } else {
-            loses ++;
-            console.log("You Lose!");
-            $('#loses').html("Loses: " + loses);
-            playGame();
-            
+                }
+            }
         }
     }
-    }
-
-
-
-
-
-    // function checkLoss()    {
-
-    // }
-
-
-
-
-
-
-
-
-    // function reset()    {
-
-    // }
-
 });
